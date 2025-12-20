bba07672eed648fca4087ac7a1e4d16ff97a3c79
peeyan <miyaoka8290@outlook.jp>
2025-12-21 02:10:51 +0900

    gg

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+//import React from 'react'
+
+function App() {
+  return (
+    <>
+      <div></div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
