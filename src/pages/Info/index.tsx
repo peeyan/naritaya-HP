25a64343852ac4a5cf6a5acfa354f20038510193
peeyan <miyaoka8290@outlook.jp>
2025-12-22 01:16:41 +0900

    店舗情報の表示内容を更新

--- src/pages/Info/index.tsx
+++ src/pages/Info/index.tsx
@@ -10,38 +10,32 @@ export default function Info() {
         <dl className="info-list">
           <div className="info-item">
             <dt>店名</dt>
-            <dd>成田屋 (Naritaya)</dd>
+            <dd>成田屋 高屋店(Naritaya-takayaten)</dd>
           </div>
 
           <div className="info-item">
             <dt>住所</dt>
-            {/* 【ここを編集】既存サイトの住所を記入 */}
             <dd>
-              〒000-0000<br />
-              （ここに住所を入力してください）<br />
-              （ビル名など）
+              〒709-0811<br />
+              岡山県赤磐市高屋 405-13
             </dd>
           </div>
 
           <div className="info-item">
             <dt>電話番号</dt>
-            {/* 【ここを編集】既存サイトの電話番号を記入 */}
-            <dd>00-0000-0000</dd>
+            <dd>086-955-8411</dd>
           </div>
 
           <div className="info-item">
             <dt>営業時間</dt>
-            {/* 【ここを編集】既存サイトの営業時間を記入 */}
             <dd>
-              昼 11:30 〜 14:00 (L.O. 13:30)<br />
-              夜 17:30 〜 22:00 (L.O. 21:00)
+              16:30 〜 22:00 (L.O. 21:30)
             </dd>
           </div>
 
           <div className="info-item">
             <dt>定休日</dt>
-            {/* 【ここを編集】既存サイトの定休日を記入 */}
-            <dd>水曜日・第一木曜日</dd>
+            <dd>月曜日</dd>
           </div>
         </dl>
       </div>
