898e9576dc2bc56486940a18b4f64fd5ca372cbd
peeyan <miyaoka8290@outlook.jp>
2025-12-22 08:58:11 +0900

    Mapの座標と電話リンクの追加

--- src/pages/Info/index.tsx
+++ src/pages/Info/index.tsx
@@ -23,7 +23,9 @@ export default function Info() {
 
           <div className="info-item">
             <dt>電話番号</dt>
-            <dd>086-955-8411</dd>
+            <dd>
+              <a href="tel:0869558411" className="tel-link">086-955-8411</a>
+            </dd>
           </div>
 
           <div className="info-item">
@@ -41,11 +43,11 @@ export default function Info() {
       </div>
 
       <div className="map-placeholder">
-        <iframe
+      <iframe
           title="Google Maps"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3282.566379967736!2d133.9472382762756!3d34.64032128647246!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3554070000000001%3A0x0!2z5oiQ55Sw5bGLIOmqnOWxi-W6lw!5e0!3m2!1sja!2sjp!4v1700000000000!5m2!1sja!2sjp"
+          src="https://maps.google.com/maps?q=〒709-0811%20岡山県赤磐市高屋%20405-13&t=&z=15&ie=UTF8&iwloc=&output=embed"
           width="100%"
-          height="400"
+          height="450"
           style={{ border: 0 }}
           allowFullScreen
           loading="lazy"
